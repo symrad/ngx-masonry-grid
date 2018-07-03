db27de176728f8831a8a66cc4d12e3b0e68be0d5
Simone Radice <simone.radice>
2018-07-03 11:49:02 +0200

    bug fix path immagini

--- demo/app/app.component.ts
+++ demo/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { Breakpoints } from '@angular/cdk/layout';
+import { APP_BASE_HREF, LocationChangeListener, LocationStrategy, PathLocationStrategy } from '@angular/common';
 import {
   trigger,
   state,
@@ -12,6 +13,7 @@ import {
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+  providers: [{provide: LocationStrategy, useClass: PathLocationStrategy}],
   animations: [
     trigger('test', [
       state('inactive', style({
@@ -36,40 +38,44 @@ import {
 export class AppComponent {
   title = 'app';
   listMasonry = [
-    {src:'../assets/1.jpg'},
-    {src:'../assets/2.jpg'},
-    {src:'../assets/3.jpg'},
-    {src:'../assets/4.jpg'},
-    {src:'../assets/5.jpg'},
-    {src:'../assets/6.jpg'},
-    {src:'../assets/7.jpg'},
-    {src:'../assets/8.jpg'},
-    {src:'../assets/9.jpg'},
-    {src:'../assets/10.jpg'},
-    {src:'../assets/11.jpg'}
+    {src:`${this.location.getBaseHref()}/assets/1.jpg`},
+    {src:`${this.location.getBaseHref()}/assets/2.jpg`},
+    {src:`${this.location.getBaseHref()}/assets/3.jpg`},
+    {src:`${this.location.getBaseHref()}/assets/4.jpg`},
+    {src:`${this.location.getBaseHref()}/assets/5.jpg`},
+    {src:`${this.location.getBaseHref()}/assets/6.jpg`},
+    {src:`${this.location.getBaseHref()}/assets/7.jpg`},
+    {src:`${this.location.getBaseHref()}/assets/8.jpg`},
+    {src:`${this.location.getBaseHref()}/assets/9.jpg`},
+    {src:`${this.location.getBaseHref()}/assets/10.jpg`},
+    {src:`${this.location.getBaseHref()}/assets/11.jpg`}
   ];
   queries = [{query:Breakpoints.Small, columns:2},{query:Breakpoints.Medium, columns:3},{query:Breakpoints.Large, columns:4}];
-  obj =  {src:'../assets/3.jpg'};
+  obj =  {src:`${this.location.getBaseHref()}/assets/3.jpg`};
   test = '';
+
+  constructor(public location: LocationStrategy){
+
+  }
   
   aggiungi(){
     this.listMasonry.push(
-      {src:'../assets/1.jpg'},
-      {src:'../assets/2.jpg'},
+      {src:`${this.location.getBaseHref()}/assets/1.jpg`},
+    {src:`${this.location.getBaseHref()}/assets/2.jpg`},
       this.obj
     );
   }
 
   appendi(){
     this.listMasonry.unshift(
-      {src:'../assets/7.jpg'},
-      {src:'../assets/8.jpg'},
-      {src:'../assets/9.jpg'}
+      {src:`${this.location.getBaseHref()}/assets/7.jpg`},
+      {src:`${this.location.getBaseHref()}/assets/8.jpg`},
+      {src:`${this.location.getBaseHref()}/assets/9.jpg`}
     );
   }
 
   modifica(){
-    this.obj.src = '../assets/11.jpg';
+    this.obj.src = `${this.location.getBaseHref()}/assets/11.jpg`;
   }
 
   rimuovi(){
